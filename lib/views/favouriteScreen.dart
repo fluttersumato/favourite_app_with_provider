d80c932f344adb1acf5924a3f3909af66201197d
Yaseen Wakeel <yaseen.wakeel@sumatosolutions.com>
2022-08-27 17:16:36 +0500

    Provider with Consumer

--- lib/views/favouriteScreen.dart
+++ lib/views/favouriteScreen.dart
@@ -1,6 +1,7 @@
 import 'package:favourite_app_with_provider/providers/FavouriteProvider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
 class FavouriteScreen extends StatefulWidget {
   const FavouriteScreen({Key? key}) : super(key: key);
 
@@ -11,7 +12,7 @@ class FavouriteScreen extends StatefulWidget {
 class _FavouriteScreenState extends State<FavouriteScreen> {
   @override
   Widget build(BuildContext context) {
-    final favouriteProvider=Provider.of<FavouriteProvider>(context);
+    // final favouriteProvider = Provider.of<FavouriteProvider>(context);
     print('build');
     return SafeArea(
       child: Scaffold(
@@ -23,16 +24,25 @@ class _FavouriteScreenState extends State<FavouriteScreen> {
         body: Column(
           children: [
             Expanded(
-              child: ListView.builder(itemCount: 100,itemBuilder: (context, index) {
-                return ListTile(
-                  onTap: (){
-                    favouriteProvider.AddIndex(index);
-                    print("clicked $index");
-                  },
-                  title: Text('item $index'),
-                  trailing: favouriteProvider.selectedItems.contains(index) ? Icon(Icons.favorite):Icon(Icons.favorite_outline),
-                );
-              },),
+              child: ListView.builder(
+                itemCount: 100,
+                itemBuilder: (context, index) {
+                  return Consumer<FavouriteProvider>(
+                      builder: (context, provider, child) {
+                    print("only this widget rebuild");
+                    return ListTile(
+                      onTap: () {
+                        provider.AddIndex(index);
+                        print("clicked $index");
+                      },
+                      title: Text('item $index'),
+                      trailing: provider.selectedItems.contains(index)
+                          ? Icon(Icons.favorite)
+                          : Icon(Icons.favorite_outline),
+                    );
+                  });
+                },
+              ),
             ),
           ],
         ),
